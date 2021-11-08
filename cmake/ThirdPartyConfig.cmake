e07eff9ffe04892905c85afd869b3e8b701e90cf
Dawson_wen <dawsonwen123@gmail.com>
2021-11-08 22:34:13 +0800

    fix thirdparty cmake script

--- cmake/ThirdPartyConfig.cmake
+++ cmake/ThirdPartyConfig.cmake
@@ -1,2 +1,11 @@
 add_subdirectory(ThirdParty/fmt-7.1.3)
 add_subdirectory(ThirdParty/yaml_cpp)
+
+include_directories(
+    ${yaml_cpp_INCLUDE_DIRS}
+    ${fmt_INCLUDE_DIRS}
+)
+link_libraries(
+    ${yaml_cpp_LIBS}
+    ${fmt_LIBS}
+)
