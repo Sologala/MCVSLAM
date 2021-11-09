81d0f163eced78518b818c7197f5d0e80cbaed81
Dawson_wen <dawsonwen123@gmail.com>
2021-11-09 09:59:31 +0800

    fix DarwinOpenCV dependency

--- cmake/DarwinOpenCV.cmake
+++ cmake/DarwinOpenCV.cmake
@@ -1,13 +1,4 @@
  
-IF (CMAKE_SYSTEM_NAME MATCHES "Linux")
-	MESSAGE(STATUS "current platform: Linux ")
-ELSEIF (CMAKE_SYSTEM_NAME MATCHES "Windows")
-	MESSAGE(STATUS "current platform: Windows")
-ELSEIF (CMAKE_SYSTEM_NAME MATCHES "FreeBSD")
-	MESSAGE(STATUS "current platform: FreeBSD")
-ELSE ()
-	MESSAGE(STATUS "other platform: ${CMAKE_SYSTEM_NAME}")
-ENDIF (CMAKE_SYSTEM_NAME MATCHES "Linux")
- 
 
 set(OpenCV_ROOT /Users/wen/anaconda3/envs/ROS/lib/cmake/opencv4)
+message(USE " " OpenCV  " " ${OpenCV_ROOT})
