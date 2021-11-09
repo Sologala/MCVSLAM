44e6f7e373182d052e5b2d7bc080e4ce16ca305f
Dawson_wen <dawsonwen123@gmail.com>
2021-11-09 17:53:23 +0800

    repace yaml-cpp to mini-yaml

--- cmake/ThirdPartyConfig.cmake
+++ cmake/ThirdPartyConfig.cmake
@@ -1,5 +1,7 @@
 add_subdirectory(ThirdParty/fmt-7.1.3)
-add_subdirectory(ThirdParty/yaml_cpp)
+set(yaml_cpp_INCLUDE_DIRS ThirdParty/mini-yaml/yaml)
+add_library(yaml_cpp ThirdParty/mini-yaml/yaml/Yaml.cpp)
+set(yaml_cpp_LIBS yaml_cpp)
 
 include_directories(
     ${yaml_cpp_INCLUDE_DIRS}
