9b8513a7a71e70feba04df2b286df5f6f7f9a70a
Dawson_wen <dawsonwen123@gmail.com>
2021-11-09 21:40:26 +0800

    stash commit

--- modules/capture/capture.hpp
+++ modules/capture/capture.hpp
@@ -5,32 +5,32 @@
 #include <string>
 namespace MCVSLAM {
 class Capture {
-private:
-public:
-  enum Work_Mode { WEBCAM = 0, VIDEO_FILE = 1, PATH = 2 };
+  private:
+  public:
+    enum Work_Mode { WEBCAM = 0, VIDEO_FILE = 1, PATH = 2 };
 
-  Capture(const std::string &_fileName);
-  Capture(const int &_camId);
-  ~Capture();
+    Capture(const std::string &_fileName){};
+    Capture(const int &_camId){};
+    ~Capture(){};
 
-  bool get(cv::Mat &img);
+    bool get(cv::Mat &img);
 
-private:
-  void Init();
+  private:
+    void Init();
 
-private:
-  int _cam_id = 0;
-  double timeStamp = 0;
-  int img_path_idx = 0;
-  double specified_duration = 0;
-  int len;
+  private:
+    int _cam_id = 0;
+    double timeStamp = 0;
+    int img_path_idx = 0;
+    double specified_duration = 0;
+    int len;
 
-  Work_Mode _mode;
+    Work_Mode _mode;
 
-  cv::VideoCapture *cap;
+    cv::VideoCapture *cap;
 
-  const std::string file_name_;
-  std::vector<std::string> img_paths;
+    const std::string file_name_;
+    std::vector<std::string> img_paths;
 };
 } // namespace MCVSLAM
 #endif
