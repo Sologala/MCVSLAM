9fa353b46ceca0db34d69c14f3584f178bc1b339
Dawson <dawsonwen123@gmail.com>
2021-11-22 23:08:34 +0800

    fix match result filter default parameter

--- modules/matcher/Matcher.cpp
+++ modules/matcher/Matcher.cpp
@@ -8,31 +8,28 @@
 #include <opencv2/features2d.hpp>
 #include <stdexcept>
 namespace MCVSLAM {
-    MatchRes& MatchRes::FilterThreshold(int thres_hold) 
-    {
-        int i = 0, j = this->size() - 1;
-        while (i <= j){
-            if ((*this)[i].distance > thres_hold){
-                (*this)[i] = (*this)[j--];
-            }
-            else{
-                i ++;
-            }
+MatchRes& MatchRes::FilterThreshold(const int thres_hold) {
+    int i = 0, j = this->size() - 1;
+    while (i <= j) {
+        if ((*this)[i].distance > thres_hold) {
+            (*this)[i] = (*this)[j--];
+        } else {
+            i++;
         }
-        (*this).resize(i);
-        return (*this);
     }
-    
-    MatchRes MatchResKnn::FilterRatio(int ratio) 
-    {
-        MatchRes res;
-        for (auto v_match : (*this)) {
-            if (v_match[0].distance / v_match[1].distance <= ratio) {
-                res.push_back(v_match[0]);
-            }
+    (*this).resize(i);
+    return (*this);
+}
+
+MatchRes MatchResKnn::FilterRatio(const float ratio) {
+    MatchRes res;
+    for (auto v_match : (*this)) {
+        if (v_match[0].distance / v_match[1].distance <= ratio) {
+            res.push_back(v_match[0]);
         }
-        return res;
     }
+    return res;
+}
 
 cv::Ptr<cv::BFMatcher> matcher_L2 = cv::BFMatcher::create(cv::NormTypes::NORM_L2);
 cv::Ptr<cv::BFMatcher> matcher_HAM = cv::BFMatcher::create(cv::NormTypes::NORM_HAMMING);
@@ -55,42 +52,35 @@ Matcher& Matcher::GetInstance(MATCH_DISTANCE _dis_mode) {
     }
 }
 
-int Matcher::HammingDistance(const cv::Mat &a, const cv::Mat &b) 
-{
-		const int *pa = a.ptr<int32_t>();
-		const int *pb = b.ptr<int32_t>();
+int Matcher::HammingDistance(const cv::Mat& a, const cv::Mat& b) {
+    const int* pa = a.ptr<int32_t>();
+    const int* pb = b.ptr<int32_t>();
 
-		int dist = 0;
+    int dist = 0;
 
-		for (int i = 0; i < 8; i++, pa++, pb++)
-		{
-			unsigned int v = *pa ^ *pb;
-			v = v - ((v >> 1) & 0x55555555);
-			v = (v & 0x33333333) + ((v >> 2) & 0x33333333);
-			dist += (((v + (v >> 4)) & 0xF0F0F0F) * 0x1010101) >> 24;
-		}
+    for (int i = 0; i < 8; i++, pa++, pb++) {
+        unsigned int v = *pa ^ *pb;
+        v = v - ((v >> 1) & 0x55555555);
+        v = (v & 0x33333333) + ((v >> 2) & 0x33333333);
+        dist += (((v + (v >> 4)) & 0xF0F0F0F) * 0x1010101) >> 24;
+    }
 
-		return dist;
+    return dist;
 
-		// ----------------use the keypoint net feature
+    // ----------------use the keypoint net feature
 }
 
-MatchRes Matcher::Match(cv::InputArray& desp1,  cv::InputArray& desp2) 
-{
+MatchRes Matcher::Match(cv::InputArray& desp1, cv::InputArray& desp2) {
     MatchRes res;
-    Get_BF_Matcher(this->dis_mode)->match(desp1, desp2,res);
+    Get_BF_Matcher(this->dis_mode)->match(desp1, desp2, res);
     return res;
 }
 
-
-
-MatchResKnn Matcher::KnnMatch(cv::InputArray& desp1,  cv::InputArray& desp2, int k) 
-{
+MatchResKnn Matcher::KnnMatch(cv::InputArray& desp1, cv::InputArray& desp2, int k) {
     MatchResKnn res;
-    Get_BF_Matcher(this->dis_mode)->knnMatch(desp1, desp2,res , k);
+    Get_BF_Matcher(this->dis_mode)->knnMatch(desp1, desp2, res, k);
     return res;
 }
-
 
 std::vector<cv::DMatch> Matcher::BFMatch(const Keypoints& kp1, const Desps& desp1, const Keypoints& kp2, const Desps& desp2) {
     std::vector<std::vector<cv::DMatch>> res;
--- modules/matcher/Matcher.hpp
+++ modules/matcher/Matcher.hpp
@@ -7,35 +7,35 @@
 
 #include "../local_feature/BaseExtractor/BaseExtractor.hpp"
 
-#define ORB_GOOD_THRESHOLD  64
+#define ORB_GOOD_THRESHOLD 64
 
 namespace MCVSLAM {
 enum MATCH_DISTANCE { HAMMING = 0, COS = 1, NORM2 = 2 };
 
-class MatchRes: public std::vector<cv::DMatch>{
-    public:
-    MatchRes& FilterThreshold(int thres_hold);
+class MatchRes : public std::vector<cv::DMatch> {
+   public:
+    MatchRes& FilterThreshold(const int thres_hold = 64);
 };
-class MatchResKnn: public std::vector<std::vector<cv::DMatch>>{
-    public:
-    MatchRes FilterRatio(int ratio);
+class MatchResKnn : public std::vector<std::vector<cv::DMatch>> {
+   public:
+    MatchRes FilterRatio(const float ratio = 0.6);
 };
 
 class Matcher {
    public:
     static Matcher& GetInstance(MATCH_DISTANCE _dis_mode);
-    static int HammingDistance(const cv::Mat &a, const cv::Mat &b);
+    static int HammingDistance(const cv::Mat& a, const cv::Mat& b);
 
    private:
     Matcher(){};
     ~Matcher(){};
 
    public:
-    MatchRes Match(cv::InputArray& desp1,  cv::InputArray& desp2);
-    MatchResKnn KnnMatch(cv::InputArray& desp1,  cv::InputArray& desp2, int k = 2);
+    MatchRes Match(cv::InputArray& desp1, cv::InputArray& desp2);
+    MatchResKnn KnnMatch(cv::InputArray& desp1, cv::InputArray& desp2, int k = 2);
     std::vector<cv::DMatch> BFMatch(const Keypoints& kp1, const Desps& desp1, const Keypoints& kp2, const Desps& desp2);
     std::vector<cv::DMatch> DBowMatch(const Keypoints& kp1, const Desps& desp1, const Keypoints& kp2, const Desps& desp2);
-    
+
    public:
     MATCH_DISTANCE dis_mode;
 };
