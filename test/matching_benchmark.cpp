f6ebcebc760fdf40393383dd49125b7d5654edea
Dawson <dawsonwen123@gmail.com>
2021-12-21 14:54:46 +0800

    Pub Tracjtoriy by ros and evaluation with python scrit

--- test/matching_benchmark.cpp
+++ test/matching_benchmark.cpp
@@ -4,6 +4,10 @@
 #include <message_filters/time_synchronizer.h>
 #include <unistd.h>
 
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <string>
+
 #include "Frame.hpp"
 #include "Map.hpp"
 #include "MapPoint.hpp"
@@ -16,11 +20,9 @@
 #include "pyp/cmdline/cmdline.h"
 #include "pyp/fmt/fmt.hpp"
 #include "pyp/timer/timer.hpp"
-#include <iostream>
-#include <opencv2/core.hpp>
-#include <string>
 using namespace std;
 using namespace MCVSLAM;
+
 std::vector<shared_ptr<Capture>> caps;
 std::vector<image_transport::Publisher> pubs;
 BaseExtractor *extractor;
@@ -30,72 +32,68 @@ int tic = 0;
 int ret = Frame::Parse("/home/wen/SLAM/MCVSLAM/config/frame.yaml");
 Map _map("/home/wen/SLAM/MCVSLAM/config/system.yaml");
 std::queue<FrameRef> frame_queue;
-Pinhole cam_left("/home/wen/SLAM/MCVSLAM/config/camleft.yaml"),
-    cam_wide("/home/wen/SLAM/MCVSLAM/config/camwide.yaml");
+Pinhole cam_left("/home/wen/SLAM/MCVSLAM/config/camleft.yaml"), cam_wide("/home/wen/SLAM/MCVSLAM/config/camwide.yaml");
 
 void track(std::vector<cv::Mat> &imgs, double time_stamp) {
-  // 1.construct Frame
-  std::vector<cv::Mat> imggrays(3);
-  for (int i = 0, sz = imgs.size(); i < sz; i++) {
-    cv::cvtColor(imgs[i], imggrays[i], cv::COLOR_BGR2GRAY);
-  }
-  KeyFrame cur_frame =
-      _map.CreateFrame(imggrays[0], imggrays[1], imggrays[2], time_stamp,
-                       &cam_left, &cam_left, &cam_wide, {});
-
-  if (frame_queue.size()) {
-    KeyFrame last_frame = frame_queue.back();
-    std::vector<cv::Mat> desp1 = {cur_frame->LEFT->desps.row(1)};
-    std::vector<cv::Mat> desp2 = {cur_frame->LEFT->desps.row(1)};
-    for (uint i = 0; i < last_frame->LEFT->desps.rows; i++) {
-      desp2.push_back(last_frame->LEFT->desps.row(i));
+    // 1.construct Frame
+    std::vector<cv::Mat> imggrays(3);
+    for (int i = 0, sz = imgs.size(); i < sz; i++) {
+        cv::cvtColor(imgs[i], imggrays[i], cv::COLOR_BGR2GRAY);
     }
-    MyTimer::Timer _("match");
-    Matcher::KnnMatch(desp1, desp2);
-  }
+    KeyFrame cur_frame = _map.CreateFrame(imggrays[0], imggrays[1], imggrays[2], time_stamp, &cam_left, &cam_left, &cam_wide, {});
 
-  while (frame_queue.size() > 3) {
-    KeyFrame pframe = frame_queue.front();
-    delete pframe;
-    frame_queue.pop();
-  }
-  frame_queue.push(cur_frame);
+    if (frame_queue.size()) {
+        KeyFrame last_frame = frame_queue.back();
+        std::vector<cv::Mat> desp1 = {cur_frame->LEFT->desps.row(1)};
+        std::vector<cv::Mat> desp2 = {cur_frame->LEFT->desps.row(1)};
+        for (uint i = 0; i < last_frame->LEFT->desps.rows; i++) {
+            desp2.push_back(last_frame->LEFT->desps.row(i));
+        }
+        MyTimer::Timer _("match");
+        Matcher::KnnMatch(desp1, desp2);
+    }
+
+    while (frame_queue.size() > 3) {
+        KeyFrame pframe = frame_queue.front();
+        frame_queue.pop();
+        delete pframe;
+    }
+    frame_queue.push(cur_frame);
 }
 
 int main(int argc, char **argv) {
-  Capture::global_capture_config.Parse("../config/capture.yaml");
-  // extractor = new SURF();
-  extractor = new ORB("../config/extractor.yaml");
-  cmdline::parser argPaser;
-  argPaser.add<string>("configPath", 'c', "config Path", false,
-                       "../config/capture.yaml");
-  argPaser.parse_check(argc, argv);
-  int fps = Capture::global_capture_config.fps;
-  for (int i = 0; i < Capture::global_capture_config.capture_cnt; i++) {
-    const string dataPath = Capture::global_capture_config.caps[i].source;
-    fmt::print("{} source : {} \n", i, dataPath);
-    if (dataPath.size() == 1 && dataPath[0] != '.') {
-      stringstream ss(dataPath);
-      int id = 0;
-      ss >> id;
-      caps.emplace_back(make_shared<Capture>(id));
-    } else {
-      caps.emplace_back(make_shared<Capture>(dataPath));
-    }
-  }
-
-  for (uint i = 0; i < 500; i++) {
-    std::vector<cv::Mat> imgs(3);
+    Capture::global_capture_config.Parse("../config/capture.yaml");
+    // extractor = new SURF();
+    extractor = new ORB("../config/extractor.yaml");
+    cmdline::parser argPaser;
+    argPaser.add<string>("configPath", 'c', "config Path", false, "../config/capture.yaml");
+    argPaser.parse_check(argc, argv);
+    int fps = Capture::global_capture_config.fps;
     for (int i = 0; i < Capture::global_capture_config.capture_cnt; i++) {
-      bool res = caps[i]->get(imgs[i]);
+        const string dataPath = Capture::global_capture_config.caps[i].source;
+        fmt::print("{} source : {} \n", i, dataPath);
+        if (dataPath.size() == 1 && dataPath[0] != '.') {
+            stringstream ss(dataPath);
+            int id = 0;
+            ss >> id;
+            caps.emplace_back(make_shared<Capture>(id));
+        } else {
+            caps.emplace_back(make_shared<Capture>(dataPath));
+        }
     }
-    track(imgs, 0.);
-  }
 
-  fmt::print("{:^20}{:^20}{:^20}\n", "item", "time(ms)", "fps");
-  for (auto p : MyTimer::Timer::COUNT) {
-    fmt::print("{:^20}{:^20.6f}{:^20.6f}\n", p.first, p.second.ms(),
-               p.second.fps());
-  }
-  return 0;
+    for (uint i = 0; i < 10; i++) {
+        std::vector<cv::Mat> imgs(3);
+        for (int i = 0; i < Capture::global_capture_config.capture_cnt; i++) {
+            bool res = caps[i]->get(imgs[i]);
+        }
+        track(imgs, 0.);
+    }
+
+    fmt::print("{:^20}{:^20}{:^20}\n", "item", "time(ms)", "fps");
+    for (auto p : MyTimer::Timer::COUNT) {
+        fmt::print("{:^20}{:^20.6f}{:^20.6f}\n", p.first, p.second.ms(), p.second.fps());
+    }
+
+    return 0;
 }
--- test/test_Tracking.cpp
+++ test/test_Tracking.cpp
@@ -6,9 +6,11 @@
 #include <unistd.h>
 
 #include <iostream>
+#include <memory>
 #include <opencv2/core.hpp>
 #include <string>
 
+#include "Converter.h"
 #include "Frame.hpp"
 #include "Map.hpp"
 #include "MapPoint.hpp"
@@ -17,17 +19,20 @@
 #include "Pinhole.hpp"
 #include "Tracker.hpp"
 #include "capture/capture.hpp"
+#include "geometry_msgs/PoseArray.h"
 #include "image_transport/subscriber.h"
 #include "osg_viewer.hpp"
 #include "pyp/cmdline/cmdline.h"
 #include "pyp/fmt/fmt.hpp"
 #include "pyp/timer/timer.hpp"
 #include "ros/init.h"
+#include "ros/publisher.h"
 using namespace std;
 using namespace MCVSLAM;
 std::vector<shared_ptr<Capture>> caps;
 std::vector<image_transport::Publisher> pubs;
-BaseExtractor *extractor;
+ros::Publisher pub_trajector;
+BaseExtractor* extractor;
 
 int idx_number_kps;
 int tic = 0;
@@ -38,17 +43,40 @@ cv::Mat velocity;
 std::queue<FrameRef> localMap;
 Pinhole cam_left("/home/wen/SLAM/MCVSLAM/config/camleft.yaml"), cam_wide("/home/wen/SLAM/MCVSLAM/config/camwide.yaml");
 Tracker tracker(&_map, &viewer, "/home/wen/SLAM/MCVSLAM/config/system.yaml");
-void track(std::vector<cv::Mat> &imgs, double time_stamp) {
+
+void Publish_Tracjtory(ros::Publisher& pub_tracj, const std::vector<cv::Mat>& Tcws);
+
+void track(std::vector<cv::Mat>& imgs, double time_stamp) {
     // 1.construct Frame
     std::vector<cv::Mat> imggrays(3);
     for (int i = 0, sz = imgs.size(); i < sz; i++) {
         cv::cvtColor(imgs[i], imggrays[i], cv::COLOR_BGR2GRAY);
     }
-    FrameRef cur_frame = _map.CreateFrame(imggrays[0], imggrays[1], imggrays[2], time_stamp, &cam_left, &cam_left, &cam_wide, {});
-    MCVSLAM::Track_State state = tracker.Track(cur_frame);
+    MCVSLAM::Track_State state = tracker.Track(imggrays[0], imggrays[1], imggrays[2], time_stamp, &cam_left, &cam_left, &cam_wide);
+    Publish_Tracjtory(pub_trajector, _map.GetAllKeyFrameForShow());
 }
 
-int main(int argc, char **argv) {
+void Publish_Tracjtory(ros::Publisher& pub_tracj, const std::vector<cv::Mat>& Tcws) {
+    geometry_msgs::PoseArray msg;
+    for (const cv::Mat& Tcw : Tcws) {
+        geometry_msgs::Pose tmp_pose;
+        cv::Mat Rcw = Tcw.rowRange(0, 3).colRange(0, 3);
+        cv::Mat tcw = Tcw.rowRange(0, 3).col(3);
+        std::vector<float> q = Converter::toQuaternion(Rcw);
+        tmp_pose.orientation.w = q[0];
+        tmp_pose.orientation.x = q[1];
+        tmp_pose.orientation.y = q[2];
+        tmp_pose.orientation.z = q[3];
+        tmp_pose.position.x = tcw.at<float>(0);
+        tmp_pose.position.y = tcw.at<float>(1);
+        tmp_pose.position.z = tcw.at<float>(2);
+        msg.poses.push_back(tmp_pose);
+    }
+    msg.header.frame_id = "mos";
+    pub_tracj.publish(msg);
+}
+
+int main(int argc, char** argv) {
     Capture::global_capture_config.Parse("../config/capture.yaml");
     // extractor = new SURF();
     extractor = new ORB("../config/extractor.yaml");
@@ -58,6 +86,8 @@ int main(int argc, char **argv) {
     argPaser.parse_check(argc, argv);
     ros::init(argc, argv, "dasfasdf");
     ros::NodeHandle nh;
+
+    pub_trajector = nh.advertise<geometry_msgs::PoseArray>("MCVSLAM/tracj", 1);
     image_transport::ImageTransport it(nh);
     int fps = Capture::global_capture_config.fps;
     for (int i = 0; i < Capture::global_capture_config.capture_cnt; i++) {
