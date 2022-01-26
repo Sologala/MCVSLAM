2967079617183564b414dc515c5b3f1e30dcef66
Dawson <dawsonwen123@gmail.com>
2022-01-26 22:34:14 +0800

    script to build g2o and cv_bridge

--- build_thirdParty.sh
+++ build_thirdParty.sh
@@ -5,8 +5,17 @@ cd g2o
 mkdir build
 cd build
 
-cmake .. -DCMAKE_INSTALL_PREFIX="../../installed" -DCMAKE_BUILD_TYPE=Debug
+cmake .. -DCMAKE_INSTALL_PREFIX="../../installed" -DCMAKE_BUILD_TYPE=Release
 make -j
 make install 
 cd ../../
 
+cd cv_bridge
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX="../../installed" -DCMAKE_BUILD_TYPE=Release
+make -j
+make install
+cd ../../
+
+cd ../
